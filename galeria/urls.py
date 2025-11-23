481018ee005897efa4c478dea066b4f1c2a34cfd
Vinicius Castro <vinicius.castro97@hotmail.com>
2025-11-23 17:02:28 -0300

    improviments

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar, login, logout
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>/', imagem, name='imagem'),
+    path('buscar', buscar, name='buscar'),
+    path('login', login, name='login'),
+    path('logout', logout, name='logout'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from galeria.models import Fotografia
 
 
+@login_required(login_url='login')
 def index(request):
     fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    
+    if "categoria" in request.GET:
+        categoria = request.GET['categoria']
+        if categoria:
+            fotografias = fotografias.filter(categoria=categoria)
+    
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
+@login_required(login_url='login')
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+@login_required(login_url='login')
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(
+                Q(nome__icontains=nome_a_buscar) | Q(categoria__icontains=nome_a_buscar)
+            )
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos')
+    
+    return render(request, 'galeria/login.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
