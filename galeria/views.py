1c06bc01c5e0fb1081f63cc5175d751692dc76be
Vinicius Castro <vinicius.castro97@hotmail.com>
2025-11-30 22:29:12 -0300

    partial project step 3

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from galeria.models import Fotografia
 
 
-@login_required(login_url='login')
 def index(request):
     fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
     
@@ -17,12 +13,10 @@ def index(request):
     
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
-@login_required(login_url='login')
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
-@login_required(login_url='login')
 def buscar(request):
     fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
 
@@ -34,22 +28,3 @@ def buscar(request):
             )
 
     return render(request, 'galeria/buscar.html', {'cards': fotografias})
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            auth_login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'Usuário ou senha inválidos')
-    
-    return render(request, 'galeria/login.html')
-
-def logout(request):
-    auth_logout(request)
-    return redirect('login')
